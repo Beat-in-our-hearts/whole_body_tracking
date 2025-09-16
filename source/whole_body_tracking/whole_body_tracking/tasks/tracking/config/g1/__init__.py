fa9bffc290990f6175879e0c04858babe3a1111a
lzx <3263760852@qq.com>
2025-09-16 22:07:02 +0800

    add smooth loc task for exp

--- source/whole_body_tracking/whole_body_tracking/tasks/tracking/config/g1/__init__.py
+++ source/whole_body_tracking/whole_body_tracking/tasks/tracking/config/g1/__init__.py
@@ -36,3 +36,18 @@ gym.register(
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:G1FlatLowFreqPPORunnerCfg",
     },
 )
+
+
+############################################
+# task for smooth loc metrics experiment #
+############################################
+
+gym.register(
+    id="SmoothLoc-Baseline-Tracking-Flat-G1-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": flat_env_cfg.G1FlatEnvCfg,
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:G1FlatPPORunnerBaselineCfg",
+    },
+)
