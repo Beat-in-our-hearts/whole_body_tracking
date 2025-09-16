caf0e0cbe0f8bce0a5555276acb02b0ef6d98e25
lzx <3263760852@qq.com>
2025-09-16 10:56:20 +0800

    init

--- scripts/csv_to_npz.py
+++ scripts/csv_to_npz.py
@@ -305,10 +305,11 @@ def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene, joi
             COLLECTION = args_cli.output_name
             run = wandb.init(project="csv_to_npz", name=COLLECTION)
             print(f"[INFO]: Logging motion to wandb: {COLLECTION}")
-            REGISTRY = "motions"
+            REGISTRY = "Motions"
             logged_artifact = run.log_artifact(artifact_or_path="/tmp/motion.npz", name=COLLECTION, type=REGISTRY)
             run.link_artifact(artifact=logged_artifact, target_path=f"wandb-registry-{REGISTRY}/{COLLECTION}")
             print(f"[INFO]: Motion saved to wandb registry: {REGISTRY}/{COLLECTION}")
+            break
 
 
 def main():
