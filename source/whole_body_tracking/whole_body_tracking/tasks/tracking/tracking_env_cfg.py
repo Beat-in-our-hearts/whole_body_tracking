16c37b174e0da0524ac765d60282ede47433de82
lzx <3263760852@qq.com>
2025-09-16 21:47:48 +0800

    init afr metrics without action scale

--- source/whole_body_tracking/whole_body_tracking/tasks/tracking/tracking_env_cfg.py
+++ source/whole_body_tracking/whole_body_tracking/tasks/tracking/tracking_env_cfg.py
@@ -97,6 +97,8 @@ class CommandsCfg:
         velocity_range=VELOCITY_RANGE,
         joint_position_range=(-0.1, 0.1),
     )
+    
+    action_fluctuation_ratio = mdp.ActionFluctuationRatioCommandCfg()
 
 
 @configclass
