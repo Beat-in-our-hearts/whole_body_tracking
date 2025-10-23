2a11ea21450227209cbb92ebb80b719be3802a85
lzx <3263760852@qq.com>
2025-10-23 09:51:57 +0800

    update for bug in `rsl_rl/ppo_lag`

--- source/whole_body_tracking/whole_body_tracking/tasks/tracking/config/g1/agents/rsl_rl_ppo_cfg.py
+++ source/whole_body_tracking/whole_body_tracking/tasks/tracking/config/g1/agents/rsl_rl_ppo_cfg.py
@@ -91,7 +91,7 @@ class SafeRL_G1FlatPPORunnerCfg(RslSafeRlOnPolicyRunnerCfg):
         constraint_threshold=0.0025,
         lagrangian_multiplier_init=0.1,
         lagrangian_multiplier_lr=1e-4,
-        lagrangian_multiplier_max=10.0,
+        lagrangian_multiplier_max=100.0,
         lagrangian_multiplier_min=0.0,
     )
     
@@ -107,8 +107,9 @@ class G1FlatSafeRLPPORunnerCfg_Lafan1(SafeRL_G1FlatPPORunnerCfg):
     def __post_init__(self):
         super().__post_init__()
         self.algorithm.constraint_threshold = 0.1
-        self.algorithm.lagrangian_multiplier_lr = 1e-5
+        self.algorithm.lagrangian_multiplier_lr = 5e-4
         self.algorithm.lagrangian_multiplier_init = 1e-1
+        self.algorithm.lagrangian_multiplier_min = self.algorithm.constraint_threshold
     
 
 #########################
